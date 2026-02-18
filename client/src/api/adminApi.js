ca53cfbb0ba27f24eff6bd2b6f73222bb757ff74
Daniyal <dani081569@gmail.com>
2026-02-19 02:14:44 +0500

    update portfolio1

--- client/src/api/adminApi.js
+++ client/src/api/adminApi.js
@@ -1,8 +1,5 @@
 import instance from "./axiosInstance";
 
 export const loginAdmin = async (email, password) => {
-  return await instance.post("/api/admin/login", {
-    email,
-    password,
-  });
+  return await instance.post("/api/admin/login", { email, password });
 };
